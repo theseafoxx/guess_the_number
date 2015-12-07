a20fa06d829bfa791e32a0f11d899b2745616394
Tanya Hrabsky <tanyahrabsky@Tanyas-MBP.home>
2015-12-06 22:39:08 -0500

    For loop created

--- 04_loops/loops.js
+++ 04_loops/loops.js
@@ -1,1 +1,11 @@
+/*
+  for loop
+ */
 
+function repeat(text, count) {
+    var textString;
+    for(var ii=0; ii<count; ii++) {
+        textString += text.toString();
+    }
+    return textString;
+}
